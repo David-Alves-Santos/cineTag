04707092f073fc34747385f698d8282aa6a81990
David-Alves-Santos <01000100avi01000100@gmail.com>
2023-10-20 17:34:44 -0300

    Primeiro Commit

---
+++ src/pages/PaginaBase/index.js
@@ -0,0 +1,21 @@
+import Cabecalho from 'componentes/Cabecalho';
+import styles from './PaginaBase.module.css'
+import FavoritosProvider from 'contextos/Favoritos';
+import Container from 'componentes/Container';
+import { Outlet } from 'react-router-dom';
+import Rodape from 'componentes/Rodape';
+
+function PaginaBase () {
+    return (
+        <main>
+            <Cabecalho />
+            <FavoritosProvider>
+                <Container>
+                    <Outlet />
+                </Container>
+            </FavoritosProvider>
+            <Rodape />
+        </main>
+    )
+}
+export default PaginaBase;
